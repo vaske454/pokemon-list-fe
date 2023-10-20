5bb86839f88714a31f9e4cdd56b167e5e80db539
vaske454 <vasilijetomovic98@gmail.com>
2023-10-20 10:32:27 +0200

    FE bug fixed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,26 +6,23 @@ import { useRouter } from 'next/router'
 export default function Home() {
     const { pokemonList } = usePokemonList()
     const perPage = 20
+    const currentPage = parseInt(pokemonList?.current_page)
+    const nextPage = currentPage + 1
+    const previousPage = currentPage - 1
 
     // Number of pages based on the total number of Pokémon and the number of Pokémon per page
     const totalPages = Math.ceil(pokemonList?.total / perPage)
     const router = useRouter()
-    const { paged } = router.query
-    const currentPageFromURL = parseInt(router.query.paged, 10) || 1
+    const currentPageFromURL = parseInt(currentPage) || 1
 
     // Local state to track the current page
-    const [currentPage] = useState(currentPageFromURL)
+    const [currentPageState] = useState(currentPageFromURL)
 
     // Calculate the offset for the current page
-    const offset = (currentPage - 1) * perPage
+    const offset = (currentPageState - 1) * perPage
 
     const handlePageChange = newPage => {
         router.push(`/?paged=${newPage}`)
-    }
-    // eslint-disable-next-line no-console
-    let pageNumber = parseInt(paged, 10)
-    if (isNaN(pageNumber)) {
-        pageNumber = 1
     }
 
     return (
@@ -53,10 +50,14 @@ export default function Home() {
                                     </h2>
                                     <table>
                                         <thead>
-                                            <th className="text-sm">
-                                                Property
-                                            </th>
-                                            <th className="text-sm">Value</th>
+                                            <tr>
+                                                <th className="text-sm">
+                                                    Property
+                                                </th>
+                                                <th className="text-sm">
+                                                    Value
+                                                </th>
+                                            </tr>
                                         </thead>
                                         <tbody>
                                             <tr>
@@ -113,34 +114,17 @@ export default function Home() {
             <footer>
                 {pokemonList?.total > 0 && (
                     <ul className="pagination flex justify-center my-4">
-                        <li>
-                            <button
-                                className={`bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-l cursor-pointer ${
-                                    pageNumber <= 1 ? 'hidden' : ''
-                                }`}
-                                onClick={() => {
-                                    const url = new URL(window.location.href)
-                                    const paged =
-                                        parseInt(
-                                            url.searchParams.get('paged'),
-                                            10,
-                                        ) || 1
-                                    if (paged > 1) {
-                                        url.searchParams.set(
-                                            'paged',
-                                            (paged - 1).toString(),
-                                        )
-                                        window.location.href = url.toString()
-                                    }
-                                }}>
-                                Previous
-                            </button>
+                        <li
+                            className={`bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-l cursor-pointer ${
+                                previousPage < 1 ? 'hidden' : ''
+                            }`}>
+                            <a href={`/?paged=${previousPage}`}>Previous</a>
                         </li>
 
                         {Array.from({ length: totalPages }, (_, index) => (
                             <li
                                 className={`px-4 py-2 ${
-                                    pageNumber === index + 1
+                                    currentPage === index + 1
                                         ? 'bg-blue-500 text-white font-bold'
                                         : 'bg-gray-200'
                                 } rounded cursor-pointer`}
@@ -153,28 +137,11 @@ export default function Home() {
                             </li>
                         ))}
 
-                        <li>
-                            <button
-                                className={`bg-blue-500 hover-bg-blue-600 text-white font-bold py-2 px-4 rounded-r cursor-pointer ${
-                                    pageNumber >= totalPages ? 'hidden' : ''
-                                }`}
-                                onClick={() => {
-                                    const url = new URL(window.location.href)
-                                    const paged =
-                                        parseInt(
-                                            url.searchParams.get('paged'),
-                                            10,
-                                        ) || 1
-                                    if (paged < totalPages) {
-                                        url.searchParams.set(
-                                            'paged',
-                                            (paged + 1).toString(),
-                                        )
-                                        window.location.href = url.toString()
-                                    }
-                                }}>
-                                Next
-                            </button>
+                        <li
+                            className={`bg-blue-500 hover-bg-blue-600 text-white font-bold py-2 px-4 rounded-r cursor-pointer ${
+                                currentPage >= totalPages ? 'hidden' : ''
+                            }`}>
+                            <a href={`/?paged=${nextPage}`}>Next</a>
                         </li>
                     </ul>
                 )}
